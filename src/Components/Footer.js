5f7fdad108dbd1730b0734ac25782ec159adc5d0
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-18 14:57:07 +0530

    Add file

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer className="bg-light py-3">
+      <Container>
+        <p className="text-center mb-0">
+          &copy; 2024 MyBlog. All Rights Reserved.
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
