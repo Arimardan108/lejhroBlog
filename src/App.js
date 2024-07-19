32fabd1c608c76a38828a538cec6a48e27affed1
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-19 20:15:55 +0530

    Add file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Router from "./Router/Router";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-export default function App() {
+import Navbar from "./Components/Navbar.js";
+function App() {
   return (
-    <div>
-      <Router />
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
+export default App;
