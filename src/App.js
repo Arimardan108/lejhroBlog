1137a93c2ab068b1d340e26b8612546708afee69
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-20 21:51:24 +0530

    Add file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
 import React from "react";
-import Navbar from "./Components/Navbar.js";
-function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
-}
-export default App;
+import "bootstrap/dist/css/bootstrap.min.css";
+function
